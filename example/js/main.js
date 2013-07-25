a632800168f6980ff463bf91cbdb510f01b9d182
Justin Yoo <justin.yoo@aliencube.com>
2013-07-25 16:32:27 +1000

    Updated example page.

--- example/js/main.js
+++ example/js/main.js
@@ -1,1 +1,22 @@
+(function ($) {
+    $(document).ready(function () {
+        $("#select-box").click(function () {
+            var value = $(this).val();
+            var cells = $("#table tbody tr[id=\"value-" + value + "\"] td");
+            var username = $(cells[1]).text();
+            var password = $(cells[2]).text();
+            var city = $(cells[3]).text();
+            var postcode = $(cells[4]).text();
 
+            $("#patched-username").val(username);
+            $("#patched-password").val(password);
+            $("#patched-city").val(city);
+            $("#patched-postcode").val(postcode);
+
+            $("#unpatched-username").val(username);
+            $("#unpatched-password").val(password);
+            $("#unpatched-city").val(city);
+            $("#unpatched-postcode").val(postcode);
+        });
+    });
+})(jQuery);
